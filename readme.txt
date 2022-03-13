de449569bf151364231d67729439b6afc27d5a82
shankarsharma13 <101489253+shankarsharma13@users.noreply.github.com>
2022-03-13 09:06:37 +0530

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,15 @@
+-the zip file contains 4 files
+-this sigin and signup Form is made by Using Php & MySQL
+-to start we have t0 open XAMPP control panel
+-and connect with by starting Apache,MySQL
+-when the module color turns green you are good to go
+-then open up the file using LocalHost
+-the folder contains the description of the files and their functionality! 
+config.php - Configure database 
+login.php - Allows user to login 
+register.php -Allows user to login 
+welcome.php - If user was able to login redirect him to welcome page 
+logout.php - Allows user to logout
+
+-the folder also contain images of application as proof..
+-please check those images out if you have any error in opening the appliciation.
---
+++ welcome.php
@@ -0,0 +1,115 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
+{
+    header("location: login.php");
+}
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>CONTACT APP</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">CONTACT APP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">SIGN UP</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">SIGNIN</a> 
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+
+      
+     
+    </ul>
+
+
+    <div class="navbar-collapse collapse">
+  <ul class="navbar-nav ml-auto">
+  <li class="nav-item active">
+        <a class="nav-link" href="#"> <img src="https://img.icons8.com/metro/26/000000/guest-male.png"> <?php echo "Welcome ". $_SESSION['username']?></a>
+      </li>
+  </ul>
+  </div>
+
+
+  </div>
+</nav>
+
+<div class="container mt-4">
+<h3><?php echo "Welcome ". $_SESSION['username']?>! You can now use this website</h3>
+<hr>
+
+</div>
+
+
+<div class="container">
+    <h2>Contact Form and Contact List Page</h2>
+    <hr>
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+    <br>
+    <br>
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    <br>
+    <br>
+
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" placeholder="Your email id..">
+    <br>
+    <br>
+
+    <label for="phone">phone number</label>
+    <input type="tel" id="phone" name="phone" placeholder="phone number..">
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <input type="submit" value="Submit">
+
+
+    
+  </form>
+</div>
+    
+
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
